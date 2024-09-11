a6cdd42fb52acc6eff95c6e72a683eac270f9689
Jorge Varela Zamora <jotaaloud@gmail.com>
2024-09-11 23:47:17 +0200

    Add files via upload

---
+++ 1DAM/Sistemas/3erTrim/practica14/test4tt13.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+int conts = 0;
+
+void tac();
+void ring();
+void tic(){
+ printf("TIC\n");
+ signal(SIGALRM,tac);
+ alarm(1);
+ conts= conts+1;
+}
+
+void tac(){
+ printf("TAC\n");
+ signal(SIGALRM,tic);
+ alarm(1);
+ conts=conts+1;
+}
+
+void ring(){
+  printf("RIIIING!!\n");
+  signal(SIGALRM,tic);
+  alarm(1);
+}
+
+int main(int argc, char *argv[]) {
+    signal(SIGALRM, tic);
+    alarm(1);
+    
+    while(1) {
+        pause();
+        if(conts%13 ==0){
+          ring();
+        }
+    }
+    return 0;
+}
+
+
