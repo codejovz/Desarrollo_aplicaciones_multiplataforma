4d2dfa001e9e714cca4fed27d79118e6435b7ae6
Jorge Varela Zamora <jotaaloud@gmail.com>
2025-01-24 14:25:04 +0100

    Video 5 and 6 added

---
+++ Por mi cuenta/Kotlin - Pildoras Informaticas/Video5/apuntes.kt
@@ -0,0 +1,12 @@
+fun main() {  //main SOLO apps de consola, porque tendran otro punto de entrada (onCreate)
+    
+    println("Hola mundo, soy Jorge")  //Esta linea de codigo imprime el mensaje 'hola mundo'
+    
+    var edad:Int=10
+    
+    var nombre:String = "Jorge"
+    
+    println(nombre)
+    println(edad)
+
+}
